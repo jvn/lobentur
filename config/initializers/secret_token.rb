15aff9368165bad051bf0fce5f9cff7b42bb133b
jvn <jvn@mikiaholding.dk>
2012-01-18 17:23:34 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lobentur::Application.config.secret_token = 'c417ac5fde559650ccc78a47d1ce1115e22999a4205f53ed7e27a6a139aa080112055bc82914a3f459741e3ba671c2afc6f6aeb2300d92a6d6b3d1d379812e6f'
